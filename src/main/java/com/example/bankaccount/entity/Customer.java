5cd2c7dd01b1b80301d55c954b560e3ff73193a2
soumesh2002 <soumesh6622@gmail.com>
2024-07-18 22:59:07 +0530

    minor changes

--- src/main/java/com/example/bankaccount/entity/Customer.java
+++ src/main/java/com/example/bankaccount/entity/Customer.java
@@ -1,8 +1,59 @@
 package com.example.bankaccount.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Customer {
     private int customerID;
     private String customerName;
     private long phoneNumber;
     private String address;
+    private Set<Account> accounts;
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Set<Account> getAccounts() {
+        return accounts;
+    }
+
+    public void setAccounts(Set<Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public void addAccount(Account account) {
+        if (this.accounts == null) {
+            this.accounts = new HashSet<>();
+        }
+        this.accounts.add(account);
+    }
 }
